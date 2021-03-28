73ba2826d5265babc6ff45513ee5eac2717d0b92
jgraykeyin <justin.gray@keyin.com>
2021-03-28 09:38:11 -0230

    Added a hambuger button

--- src/Components/Titlebar.js
+++ src/Components/Titlebar.js
@@ -1,6 +1,11 @@
 function Titlebar() {
     return (
       <header className="Titlebar">
+        <div className="HamburgerBtn">
+          <div className="BurgerLine"></div>
+          <div className="BurgerLine"></div>
+          <div className="BurgerLine"></div>
+        </div>
         <p>Dunder Mifflin Trivia</p>
       </header>
     )
